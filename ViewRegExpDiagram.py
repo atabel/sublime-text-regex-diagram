075dc4e6f3e2e11a4014484f796c669ae9ec5bbf
atabel <atabel87@gmail.com>
2013-10-19 19:03:07 +0200

    Changed selection method

--- ViewRegExpDiagram.py
+++ ViewRegExpDiagram.py
@@ -1,4 +1,3 @@
-import sublime
 import sublime_plugin
 import webbrowser
 import urllib
@@ -16,28 +15,8 @@ class ViewRegExpDiagramCommand(sublime_plugin.TextCommand):
 
     def selection(self):
         s = self.view.sel()[0]
-
-        start = s.a
-        end = s.b
-
-        # if nothing is selected, expand selection to nearest terminators
-        if start == end:
-            view_size = self.view.size()
-            terminator = '/\"\''
-
-            # move the selection back to the start of the RegExp
-            while (start > 0
-                    and not self.view.substr(start - 1) in terminator
-                    and self.view.classify(start) & sublime.CLASS_LINE_START == 0):
-                start -= 1
-
-            # move end of selection forward to the end of the RegExp
-            while (end < view_size
-                    and not self.view.substr(end) in terminator
-                    and self.view.classify(end) & sublime.CLASS_LINE_END == 0):
-                end += 1
-
-        return self.view.substr(sublime.Region(start, end)).strip()
+        scope_reg = self.view.extract_scope(s.a) if s.a == s.b else s
+        return self.view.substr(scope_reg)
 
     def strip_quotes(self, regexp):
         if (
